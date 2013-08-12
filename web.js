0e14409e48a57ae623ce9b52f70e864e60e2280e
dombiv <chopdichopdi@gmail.com>
2013-08-12 01:38:31 +0000

    use buf.tostring and readfilesnc

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  var buf = fs.readFileSync('index.html');
+
+  response.send(buf.toString());
+//  response.send('Hello World 3!');
 });
 
 var port = process.env.PORT || 5000;
